093c5133ab84a6572ccd0d740286243965d1d1b0
Matt Wass <mattywass@btinternet.com>
2024-12-01 22:46:55 +0000

    add_ship method added to python code

--- run.py
+++ run.py
@@ -33,4 +33,10 @@ class Board:
         else:
             return "Miss"
 
-    def add_ships
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships")
+            else:
+                self.ships.append((x, y))
+                if self.type == "player":
+                    self.board[x][y] = "@"
